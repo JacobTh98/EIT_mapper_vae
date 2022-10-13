423c45e168ae2c6ff8433e6d1a07d88521c90d67
JacobTh98 <jacob.thoenes@googlemail.com>
2022-10-13 10:31:29 +0200

    Adjust to functions.

--- train_vae.py
+++ train_vae.py
@@ -1,6 +1,5 @@
-from select import epoll
 from datagenerator import DataGenerator
-from extra_fcts import *
+from extra_fcts import check_train_data, read_integer, read_dirs
 from vae_model import vae_model
 import os
 import numpy as np
@@ -10,7 +9,7 @@ from tensorflow.keras.optimizers import Adam
 vae = vae_model()
 vae.compile(optimizer=Adam())
 
-fpath = read_dirs("data/")
+fpath = read_dirs("data/", "Which data do you want to train with?")
 
 # initialize data generators
 params = {"path": fpath, "dim": 2807, "batch_size": 32, "shuffle": True}
@@ -20,7 +19,7 @@ range = read_integer("How many samples do you want to train?\t")
 idx = np.arange(0, range)
 np.random.shuffle(idx)
 
-print("\tPreperate samples for training...")
+print("\tPreparing samples for training...")
 training_generator = DataGenerator(idx, **params)
 
 epochs = read_integer("How many epochs do you want to train?\t")
